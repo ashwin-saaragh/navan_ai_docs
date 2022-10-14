f4d0aa439ed2900c554cd2db491eacdefbbbbb85
ashwin-saaragh <ashwin@saaragh.com>
2022-10-14 16:04:52 +0530

    Delete scratch.py

--- prediction code/scratch.py
+++
@@ -1,4 +0,0 @@
-lines = [line.replace("\n","") for line in open("E:\DEV\\navan_ai_docs\prediction code\classes.names","r").readlines()]
-classes = {i:lines[i] for i in range(len(lines))}
-print(classes)
-
